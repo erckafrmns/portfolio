d16c7a5a266b52b2fe3afa7e21e4a93d624409fb
Ericka <formaneserickajoy@gmail.com>
2024-12-05 16:06:04 +0800

    style: improved skills section styles

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -15,53 +15,55 @@ const Skills = () => {
                         <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
                     </svg>
                 </div>
-                <div className='skills-left'>
+
+                <div className="skill-content">
                     <h1>Skills</h1>
-                    {/* <div class="vertical-line"></div> */}
+                    <p>I enjoy coding and building programs, especially web applications. From designing seamless user experiences to ensuring everything functions smoothly behind the scenes, I find joy in every step of the process. <br /> Along the way, here are some languages and tools I've worked with: </p>
+                
+                    <div className="skill-list">
+
+                        <div className="skill-list-items">
+                            <div className='languages-list'>
+                                <ul>
+                                    {skills.languages.map((skill, index) => (
+                                        <li key={index}>
+                                            <img src={skill.img} alt={skill.name} />
+                                            <span>{skill.name}</span>
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
+                        </div>
+
+                        <div className="skill-list-items">
+                            <div className='frameworks-list'>
+                                <ul>
+                                    {skills.frameworks.map((skill, index) => (
+                                        <li key={index}>
+                                            <img src={skill.img} alt={skill.name} />
+                                            <span>{skill.name}</span>
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
+                        </div>
+
+                        <div className="skill-list-items">
+                            <div className='others-list'>
+                                <ul>
+                                    {skills.tools.map((skill, index) => (
+                                        <li key={index}>
+                                            <img src={skill.img} alt={skill.name} />
+                                            <span>{skill.name}</span>
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
 
-                <div className='skills-right'>
-                    <div className='languages'>
-                        {/* <h1>Languages</h1> */}
-                        <div className='languages-list'>
-                            <ul>
-                                {skills.languages.map((skill, index) => (
-                                    <li key={index}>
-                                        <img src={skill.img} alt={skill.name} />
-                                        <span>{skill.name}</span>
-                                    </li>
-                                ))}
-                            </ul>
-
-                        </div>
-                    </div>
-                    <div className='frameworks'>
-                        {/* <h1>Frameworks</h1> */}
-                        <div className='frameworks-list'>
-                            <ul>
-                                {skills.frameworks.map((skill, index) => (
-                                    <li key={index}>
-                                        <img src={skill.img} alt={skill.name} />
-                                        <span>{skill.name}</span>
-                                    </li>
-                                ))}
-                            </ul>
-                        </div>
-                    </div>
-                    <div className='others'>
-                        {/* <h1>Other Tools</h1> */}
-                        <div className='others-list'>
-                            <ul>
-                                {skills.tools.map((skill, index) => (
-                                    <li key={index}>
-                                        <img src={skill.img} alt={skill.name} />
-                                        <span>{skill.name}</span>
-                                    </li>
-                                ))}
-                            </ul>
-                        </div>
-                    </div>
-                </div>
             </section>
         </>
     )
