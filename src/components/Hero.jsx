40bf0bbf3cde47dabb8c8894cd9f010a1453d2fa
Ericka <formaneserickajoy@gmail.com>
2025-01-12 00:02:08 +0800

    style: hero, project, and skills responsiveness

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,22 +14,22 @@ const Hero = () => {
 
     useGSAP(() => {
         gsap.from(".hero-middle", {
-            y: 200,          
-            opacity: 0,     
-            duration: 1.5,     
+            y: 200,
+            opacity: 0,
+            duration: 1.5,
             ease: 'power2.out',
         });
 
         gsap.from(".hero-right", {
-            x: 200,          
-            opacity: 0,     
-            duration: 1.5,     
+            x: 200,
+            opacity: 0,
+            duration: 1.5,
             ease: 'power2.out'
         });
     });
 
     // Destructure data
-    const { firstname, roles, intro, socials, featuredProjects } = data; 
+    const { firstname, roles, intro, socials, featuredProjects } = data;
 
     // For Typewriter effect
     const [currentRole, setCurrentRole] = useState('');
@@ -38,18 +38,18 @@ const Hero = () => {
 
     useEffect(() => {
         if (charIndex < roles[roleIndex].length) {
-        const timeout = setTimeout(() => {
-            setCurrentRole((prev) => prev + roles[roleIndex].charAt(charIndex));
-            setCharIndex(charIndex + 1);
-        }, 100); 
-        return () => clearTimeout(timeout);
+            const timeout = setTimeout(() => {
+                setCurrentRole((prev) => prev + roles[roleIndex].charAt(charIndex));
+                setCharIndex(charIndex + 1);
+            }, 100);
+            return () => clearTimeout(timeout);
         } else {
-        const timeout = setTimeout(() => {
-            setCharIndex(0);
-            setCurrentRole('');
-            setRoleIndex((prev) => (prev + 1) % roles.length); 
-        }, 1500); 
-        return () => clearTimeout(timeout);
+            const timeout = setTimeout(() => {
+                setCharIndex(0);
+                setCurrentRole('');
+                setRoleIndex((prev) => (prev + 1) % roles.length);
+            }, 1500);
+            return () => clearTimeout(timeout);
         }
     }, [charIndex, roleIndex, roles]);
 
@@ -58,7 +58,7 @@ const Hero = () => {
         <>
             <section className='hero' id='hero'>
                 <div ref={heroContent} className='hero-content'>
-                    
+
                     <div className="hero-left">
 
                         <div className='hero-left-content'>
@@ -66,17 +66,16 @@ const Hero = () => {
                             <h3>I am a <span>{currentRole}<span className="cursor"></span></span></h3>
                             <p>{intro}</p>
                             <div className='hero-buttons'>
-                                <button className='LinkedInBTN' onClick={() => window.open(socials[2].link, '_blank')}><FaLinkedin className='hs-icons'/> {socials[2].name}</button>
-                                <button className='GitHubBTN' onClick={() => window.open(socials[3].link, '_blank')}><FaGithub className='hs-icons'/> {socials[3].name}</button>
+                                <button className='LinkedInBTN' onClick={() => window.open(socials[2].link, '_blank')}><FaLinkedin className='hs-icons' /> {socials[2].name}</button>
+                                <button className='GitHubBTN' onClick={() => window.open(socials[3].link, '_blank')}><FaGithub className='hs-icons' /> {socials[3].name}</button>
                             </div>
                         </div>
-                        
+
                     </div>
 
                     <div className='hero-right'>
-                        {/* <h2>Some of my projects:</h2> */}
                         <div className="slider">
-                            
+
                             <div className="slide-track">
                                 {featuredProjects.map((project, index) => (
                                     <div className="slide" key={index}>
@@ -91,7 +90,7 @@ const Hero = () => {
                                 ))}
                             </div>
                         </div>
-                       
+
                     </div>
                 </div>
                 <div class="custom-shape-divider-hero-bottom">
@@ -99,7 +98,7 @@ const Hero = () => {
                         <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="shape-fill"></path>
                     </svg>
                 </div>
-                
+
             </section>
         </>
     )
